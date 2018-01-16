77452ec6b1477755da55727c4312cb007a2c8a7f
n.fontes <n.fontes@student.isartdigital.com>
2018-01-16 14:26:59 +0100

    Update files for 4_18

--- Source/ShooterMultiEditor.Target.cs
+++ Source/ShooterMultiEditor.Target.cs
@@ -5,21 +5,9 @@ using System.Collections.Generic;
 
 public class ShooterMultiEditorTarget : TargetRules
 {
-	public ShooterMultiEditorTarget(TargetInfo Target)
+	public ShooterMultiEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-	}
-
-	//
-	// TargetRules interface.
-	//
-
-	public override void SetupBinaries(
-		TargetInfo Target,
-		ref List<UEBuildBinaryConfiguration> OutBuildBinaryConfigurations,
-		ref List<string> OutExtraModuleNames
-		)
-	{
-		OutExtraModuleNames.AddRange( new string[] { "ShooterMulti" } );
+		ExtraModuleNames.Add("ShooterMulti");
 	}
 }
